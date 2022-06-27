a7186b685d8367a15b85a4986477fff85d0d5670
goran9999 <gutovicgoran@gmail.com>
2022-06-27 22:15:04 +0200

    service interfaces for remaining entities

---
+++ spring-courses/src/main/java/rs/fon/silab/service/ProfessorService.java
@@ -0,0 +1,14 @@
+package rs.fon.silab.service;
+
+import java.util.List;
+
+import rs.fon.silab.dto.ProfessorDto;
+
+
+public interface ProfessorService {
+	ProfessorDto saveProfessor(ProfessorDto professor);
+	List<ProfessorDto> getAllProfessors();
+	ProfessorDto getProfessor(Long id);
+	ProfessorDto updateProfessor(ProfessorDto professor);
+	boolean deleteProfessor(Long id);
+}
