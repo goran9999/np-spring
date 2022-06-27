da46aa7c215a1adcac7fec66908a5d6263ce8a8e
goran9999 <gutovicgoran@gmail.com>
2022-06-27 21:15:23 +0200

    create initial spring app structure

---
+++ spring-courses/src/main/java/rs/fon/silab/SpringCoursesApplication.java
@@ -0,0 +1,19 @@
+package rs.fon.silab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "rs.fon.silab")
+@EntityScan("rs.fon.silab.model")
+@EnableJpaRepositories("rs.fon.silab.repository")
+public class SpringCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoursesApplication.class, args);
+	}
+
+}
---
+++ spring-courses/src/test/java/rs/fon/silab/springcourses/SpringCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.fon.silab.springcourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
