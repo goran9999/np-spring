68cac72027e8a4caeddb9baa4b157f6d85acf1d7
goran9999 <gutovicgoran@gmail.com>
2022-07-01 20:40:59 +0200

    add javadoc comments in area and course model classes

--- spring-courses/src/main/java/rs/fon/silab/model/Area.java
+++ spring-courses/src/main/java/rs/fon/silab/model/Area.java
@@ -9,22 +9,49 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+/**
+ * 
+ * @author goran Class that represents area of specific course
+ */
 @Entity(name = "area")
 public class Area {
+	/**
+	 * Id of area
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	/**
+	 * Name of specific area
+	 */
 	private String areaName;
+	/**
+	 * Total amount of classes which attend course with specific area
+	 */
 	private int classesCount;
-	
+
+	/**
+	 * Course to which area is related
+	 */
 	@ManyToOne
 	@JoinColumn(name = "courseId")
 	Course course;
 
+	/**
+	 * Non parameter constructor
+	 */
 	public Area() {
 		super();
 	}
 
+	/**
+	 * Constructor with parameters
+	 * 
+	 * @param id
+	 * @param areaName
+	 * @param classesCount
+	 * @param course
+	 */
 	public Area(Long id, String areaName, int classesCount, Course course) {
 		super();
 		this.id = id;
@@ -33,51 +60,83 @@ public class Area {
 		this.course = course;
 	}
 
+	/**
+	 * Returns id of area
+	 * @return
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * Sets id of area
+	 * @param id
+	 */
 	public void setId(Long id) {
-		if(id==null) {
+		if (id == null) {
 			throw new NullPointerException();
 		}
-		if(id<0) {
+		if (id < 0) {
 			throw new IllegalArgumentException();
 		}
 		this.id = id;
 	}
 
+	/**
+	 * Returns name of area
+	 * @return
+	 */
 	public String getAreaName() {
 		return areaName;
 	}
 
+	/**
+	 * Sets name of area
+	 * @param areaName
+	 */
 	public void setAreaName(String areaName) {
-		if(areaName==null) {
+		if (areaName == null) {
 			throw new NullPointerException("Name can not be null!");
 		}
-		if(areaName.trim()=="") {
+		if (areaName.trim() == "") {
 			throw new IllegalArgumentException("Name can not be empty!");
 		}
 		this.areaName = areaName;
 	}
 
+	/**
+	 * Returns number of classes for area
+	 * @return
+	 */
 	public int getClassesCount() {
 		return classesCount;
 	}
 
+	/**
+	 * Sets or updates number of classes of area
+	 * @param classesCount
+	 */
 	public void setClassesCount(int classesCount) {
-		if(classesCount<0) {
+		if (classesCount < 0) {
 			throw new IllegalArgumentException("Classes count can not be less than 0!");
 		}
 		this.classesCount = classesCount;
 	}
 
+	/**
+	 * Returns course area is related to
+	 * @return
+	 */
 	public Course getCourse() {
 		return course;
 	}
 
+	/**
+	 * Sets course related to area
+	 * @param course
+	 */
 	public void setCourse(Course course) {
-		if(course.getId()==null) {
+		if (course.getId() == null) {
 			throw new NullPointerException("Course can not be null");
 		}
 		this.course = course;
@@ -88,6 +147,9 @@ public class Area {
 		return Objects.hash(areaName, classesCount, course, id);
 	}
 
+	/**
+	 * Checks if two area objects are equal
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -101,14 +163,13 @@ public class Area {
 				&& Objects.equals(course, other.course) && Objects.equals(id, other.id);
 	}
 
+	/**
+	 * Returns string representation of area object
+	 */
 	@Override
 	public String toString() {
 		return "Area [id=" + id + ", areaName=" + areaName + ", classesCount=" + classesCount + ", course=" + course
 				+ "]";
 	}
-	
-	
-	
-	
 
 }
--- spring-courses/src/main/java/rs/fon/silab/model/Course.java
+++ spring-courses/src/main/java/rs/fon/silab/model/Course.java
@@ -18,39 +18,86 @@ import javax.persistence.OneToMany;
 
 import rs.fon.silab.util.Semester;
 
+/**
+ * 
+ * @author goran
+ *
+ * Class representing course entity in database
+ */
 @Entity
 @Table(name = "course")
 public class Course {
 
+	/**
+	 * Id of course
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	/**
+	 * Name of course
+	 */
 	@Column(name = "course_name")
 	private String courseName;
+	/**
+	 * Semmester during which course is active
+	 */
 	private Semester semester;
+	/**
+	 * Start date of course
+	 */
 	@Column(name = "startDate")
 	private Date startDate;
+	/**
+	 * End date of course
+	 */
 	@Column(name = "endDate")
 	private Date endDate;
 	@Column(name = "imageUrl")
+	/**
+	 * Logo image of course
+	 */
 	private String imageUrl;
 	@Column(name = "groupsCount")
+	/**
+	 * Total amount of groups related to course
+	 */
 	private int groupsCount;
+	/**
+	 * Level of course
+	 */
 	private String level;
+	/**
+	 * List of groups that are related to course
+	 */
 	@OneToMany(cascade = CascadeType.ALL, targetEntity = Group.class)
 	Set<Group> groups;
+	/**
+	 * List of areas related to course
+	 */
 	@OneToMany(cascade = CascadeType.ALL, targetEntity = Area.class)
 	Set<Area> areas;
 
+	/**
+	 * Non parameter constructor
+	 */
 	public Course() {
 		super();
 	}
 
+	/**
+	 * Returns logo image of course
+	 * @return
+	 */
 	public String getImageUrl() {
 		
 		return imageUrl;
 	}
 
+	/**
+	 * Sets or updateds logo image of course
+	 * @param imageUrl
+	 */
 	public void setImageUrl(String imageUrl) {
 		if(imageUrl==null) {
 			throw new NullPointerException("Image url can not be null!");
@@ -61,6 +108,17 @@ public class Course {
 		}
 	}
 
+	/**
+	 * Parameter constructor
+	 * @param id
+	 * @param courseName
+	 * @param semester
+	 * @param startDate
+	 * @param endDate
+	 * @param imageUrl
+	 * @param groupsCount
+	 * @param level
+	 */
 	public Course(Long id, String courseName, Semester semester, Date startDate, Date endDate, String imageUrl,
 			int groupsCount, String level) {
 		super();
@@ -75,10 +133,18 @@ public class Course {
 
 	}
 
+	/**
+	 * Returns id of course
+	 * @return
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * Sets id of course
+	 * @param id
+	 */
 	public void setId(Long id) {
 		if(id==null) {
 			throw new NullPointerException();
@@ -89,11 +155,19 @@ public class Course {
 		this.id = id;
 	}
 
+	/**
+	 * Returns name of course
+	 * @return
+	 */
 	public String getCourseName() {
 
 		return courseName;
 	}
 
+	/**
+	 * Sets name of course
+	 * @param courseName
+	 */
 	public void setCourseName(String courseName) {
 		if (courseName == null || courseName.trim() == "") {
 			throw new IllegalArgumentException();
@@ -101,10 +175,18 @@ public class Course {
 		this.courseName = courseName;
 	}
 
+	/**
+	 * Returns semester during which course is active
+	 * @return
+	 */
 	public Semester getSemester() {
 		return semester;
 	}
 
+	/**
+	 * Sets semester for course
+	 * @param semester
+	 */
 	public void setSemester(Semester semester) {
 		if(semester==null) {
 			throw new NullPointerException("Semester can not be null");
@@ -112,10 +194,18 @@ public class Course {
 		this.semester = semester;
 	}
 
+	/**
+	 * Returns start date of course
+	 * @return
+	 */
 	public Date getStartDate() {
 		return startDate;
 	}
 
+	/**
+	 * Sets start date of course
+	 * @param startDate
+	 */
 	public void setStartDate(Date startDate) {
 		if (startDate == null) {
 			throw new IllegalArgumentException();
@@ -123,10 +213,18 @@ public class Course {
 		this.startDate = startDate;
 	}
 
+	/**
+	 * Returns end date of course
+	 * @return
+	 */
 	public Date getEndDate() {
 		return endDate;
 	}
 
+	/**
+	 * Sets end date of course
+	 * @param endDate
+	 */
 	public void setEndDate(Date endDate) {
 		if (endDate == null || endDate.getTime() < new Date().getTime()) {
 			throw new IllegalArgumentException("End date can not be in past!");
@@ -148,25 +246,35 @@ public class Course {
 		this.groupsCount = groupsCount;
 	}
 
+	/**
+	 * Returns level of course
+	 * @return
+	 */
 	public String getLevel() {
 		return level;
 	}
-
+	
+	/**
+	 * Sets level of course
+	 * @param level
+	 */
 	public void setLevel(String level) {
 		this.level = level;
 	}
 
+	/**
+	 * String representation of course object
+	 */
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", courseName=" + courseName + ", semester=" + semester + ", startDate=" + startDate
 				+ ", endDate=" + endDate + ", groupsCount=" + groupsCount + ", level=" + level + "]";
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(courseName, endDate, groupsCount, id, level, semester, startDate);
-	}
 
+	/**
+	 * Checks if two course objects are equal
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
